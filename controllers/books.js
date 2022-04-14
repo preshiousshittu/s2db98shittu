05fbc10c180babcdeb0cbf33cecbddcf39d38aec
preshiousshittu <54418932+preshiousshittu@users.noreply.github.com>
2022-04-14 17:24:41 -0500

    lab stuff

--- controllers/books.js
+++ controllers/books.js
@@ -1,6 +1,6 @@
+const book = require('../models/book');
 var Book = require('../models/book'); 
  
-// List of all Costumes 
 exports.book_list  = async function(req, res) { 
     try{ 
         theBooks = await Book.find(); 
@@ -10,27 +10,58 @@ exports.book_list  = async function(req, res) {
         res.status(500); 
         res.send(`{"error": ${err}}`); 
     }   
-    res.send('NOT IMPLEMENTED: Costume list'); 
 }; 
  
-// for a specific Costume. 
-exports.book_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Costume detail: ' + req.params.id); 
+exports.book_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await Book.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
 }; 
  
-// Handle Costume create on POST. 
-exports.book_create_post = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Costume create POST'); 
+exports.book_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new Book(); 
+    document.name = req.body.name; 
+    document.cost = req.body.cost; 
+    document.size = req.body.size; 
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
+    res.send('NOT IMPLEMENTED: book create POST'); 
 }; 
  
-// Handle Costume delete form on DELETE. 
 exports.book_delete = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Costume delete DELETE ' + req.params.id); 
+    res.send('NOT IMPLEMENTED: book delete DELETE ' + req.params.id); 
 }; 
  
-// Handle Costume update form on PUT. 
-exports.book_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Costume update PUT' + req.params.id); 
+exports.book_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+    ${JSON.stringify(req.body)}`) 
+        try { 
+            let toUpdate = await book.findById( req.params.id) 
+            // Do updates of properties 
+            if(req.body.name)  
+                   toUpdate.name = req.body.name; 
+            if(req.body.cost) toUpdate.length = req.body.length; 
+            if(req.body.size) toUpdate.author = req.body.author; 
+            let result = await toUpdate.save(); 
+            console.log("Sucess " + result) 
+            res.send(result) 
+        } catch (err) { 
+            res.status(500) 
+            res.send(`{"error": ${err}: Update for id ${req.params.id} 
+    failed`); 
+        } 
 }; 
 
 exports.book_view_all_Page = async function(req, res) { 
--- routes/books.js
+++ routes/books.js
@@ -2,11 +2,9 @@ var express = require('express');
 const book_controllers= require('../controllers/books'); 
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('books', { title: 'Search Results books' });
-});
+
 
 /* GET costumes */ 
 router.get('/', book_controllers.book_view_all_Page ); 
+router.get('/books/:id', book_controllers.book_detail); 
 module.exports = router; 
