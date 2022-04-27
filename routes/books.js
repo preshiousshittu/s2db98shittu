a0a121dea4c41b591552d029ae4486abba063f12
preshiousshittu <54418932+preshiousshittu@users.noreply.github.com>
2022-04-27 11:10:23 -0500

    adds logging in and register

--- routes/books.js
+++ routes/books.js
@@ -4,10 +4,10 @@ var router = express.Router();
 
 
 
-/* GET costumes */ 
 router.get('/', book_controllers.book_view_all_Page ); 
 router.get('/books/:id', book_controllers.book_detail); 
 router.get('/detail', book_controllers.book_view_one_Page); 
 router.get('/create', book_controllers.book_create_Page); 
-router.get('/update', book_controllers.book_update_Page); 
+router.get('/update', secured,book_controllers.book_update_Page); 
+router.get('/delete', book_controllers.book_delete_Page); 
 module.exports = router; 
